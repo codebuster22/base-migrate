eae6b4fcf40d6b770c7abd21a65596ffef5e6a16
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-03-21 03:42:43 +0530

    feat: add enviornment variables and set network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import { config as dotConfig } from "dotenv";
+dotConfig({});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    base: {
+      url: "https://rpc.ankr.com/arbitrum",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    baseTestnet: {
+      url: "https://rpc.ankr.com/arbitrum",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_API_KEY || "",
+      baseSepolia: process.env.BASESCAN_API_KEY || "",
+      ethereum: process.env.ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || ""
+    },
+  },
 };
 
 export default config;
