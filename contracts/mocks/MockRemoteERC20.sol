2ee3406d49782fb6c35f051d8162139af6bff695
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-03-21 03:05:44 +0530

    fix: mint tokens in constructor

--- contracts/mocks/MockRemoteERC20.sol
+++ contracts/mocks/MockRemoteERC20.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockRemoteERC20 is ERC20("Mock", "MOCK") {
-    function mint(uint256 _amount) external {
-        _mint(msg.sender, _amount);
+    constructor() {
+        _mint(msg.sender, 100_000_000 ether);
     }
 }
